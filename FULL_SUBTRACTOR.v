926e84ffd1fc7289ad451f269983763580c94f5c
ashwinbaalaji <ashwinbaalaji2@gmail.com>
2025-11-28 10:08:20 +0530

    Add files via upload

---
+++ FULL_SUBTRACTOR.v
@@ -0,0 +1,12 @@
+// Full Subtractor in Verilog
+module FULL_SUBTRACTOR (
+    input  wire a, b, bin,       // Inputs
+    output wire diff, borrow     // Outputs
+);
+
+    // Logic equations
+    assign diff   = a ^ b ^ bin;                  // Difference
+    assign borrow = (~a & b) | (~(a ^ b) & bin);  // Borrow logic
+
+endmodule
+
